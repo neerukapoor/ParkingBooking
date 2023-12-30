291e5ac61fe32887195a6c1157dc8f66c82e65e6
Neeru Kapoor <467neeru@gmail.com>
2023-12-31 00:22:38 +0530

    Initializing project

---
+++ src/App.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner scan = new Scanner(System.in);
+        String name = scan.nextLine();
+        System.out.println(name);
+    }
+}
